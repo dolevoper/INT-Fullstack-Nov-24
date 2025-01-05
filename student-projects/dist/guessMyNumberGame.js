ff9fd800bb7131541794853b4d5fd1d7c9d1ee91
Guy <Guy9502@Gmail.com>
2025-01-05 23:12:46 +0200

    Improve input validation for "Guess My Number" game to ensure valid user entries for number and tries

--- student-projects/dist/guessMyNumberGame.js
+++ student-projects/dist/guessMyNumberGame.js
@@ -3,16 +3,20 @@ var randomNumber = Math.floor(Math.random() * 10) + 1;
 // Get a number and tries from the user
 var userNumber = prompt('Please enter a number between 1 and 10');
 var numberOfTries = prompt('Please enter the number of tries');
-if (userNumber === null || !Number(userNumber)) {
-    alert('Please enter a valid number between 1 and 10');
-}
-else if (numberOfTries === null || !Number(numberOfTries)) {
-    alert('Please enter a valid number of tries');
+// Check if the user number and tries are valid and make user to enter them again if they are not null
+if (userNumber !== null || numberOfTries !== null) {
+    while (!Number(userNumber)) {
+        userNumber = prompt('Please enter a number between 1 and 10');
+    }
+    while (!Number(numberOfTries)) {
+        numberOfTries = prompt('Please enter the number of tries');
+    }
 }
 // Check if the user number is the same as the random number and output the result
 for (var i = 1; i <= Number(numberOfTries); i++) {
-    if (i > (Number(numberOfTries) + 1))
+    if (i > (Number(numberOfTries) + 1)) {
         break;
+    }
     if (Number(userNumber) > randomNumber) {
         alert('Your number is higher than the random number');
         userNumber = prompt('Please enter a higher number');
--- student-projects/guessMyNumberGame.ts
+++ student-projects/guessMyNumberGame.ts
@@ -6,17 +6,21 @@ const randomNumber: number = Math.floor(Math.random() * 10) + 1;
 let userNumber: string | null = prompt('Please enter a number between 1 and 10');
 let numberOfTries: string | null = prompt('Please enter the number of tries');
 
-if (userNumber === null || !Number(userNumber)){
-    alert('Please enter a valid number between 1 and 10');
-}
-else if (numberOfTries === null || !Number(numberOfTries)){
-    alert('Please enter a valid number of tries');
+// Check if the user number and tries are valid and make user to enter them again if they are not null
+if(userNumber !== null || numberOfTries !== null){
+    while(!Number(userNumber)){
+        userNumber = prompt('Please enter a number between 1 and 10');
+    }
+    while(!Number(numberOfTries)){
+        numberOfTries = prompt('Please enter the number of tries');
+    }
 }
 
 // Check if the user number is the same as the random number and output the result
 for (let i = 1; i <= Number(numberOfTries); i++) {
-    if (i > (Number(numberOfTries)+1))
+    if (i > (Number(numberOfTries)+1)){
         break;
+    }
     if(Number(userNumber) > randomNumber){
         alert('Your number is higher than the random number');
         userNumber=prompt('Please enter a higher number');
