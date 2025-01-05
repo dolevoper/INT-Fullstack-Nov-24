b24fb2258c416e8a0c2dd4e4c5b0503018e87a46
gregoryb79 <gregoryb79@gmail.com>
2025-01-05 20:52:53 +0200

    calculator

---
+++ student-projects/gregory/Lecture 13/calculator/app.ts
@@ -0,0 +1,17 @@
+alert("This is a CALCULATOR");
+
+let variabel1 = Number(prompt("Please enter first Number"));
+let operator = prompt("Please enter the action you want '+' or '-' or '/' or '*'.");
+let variabel2 = Number(prompt("Please enter first Number"));
+
+if (operator === "+"){
+    alert(variabel1 + variabel2);
+} else if (operator === "-"){
+    alert(variabel1 - variabel2);
+} else if (operator === "/"){
+    alert(variabel1 / variabel2);
+}else if (operator === "*"){
+    alert(variabel1 * variabel2);
+}
+
+
