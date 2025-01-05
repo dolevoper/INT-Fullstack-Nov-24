463146f9fcd44d39d6137c21c0908e7517645809
Mental-potato <153oorrii153@gmail.com>
2025-01-05 19:38:55 +0200

    ts valid right angle

--- module-ii/02-05012025-conditions/app.ts
+++ module-ii/02-05012025-conditions/app.ts
@@ -4,4 +4,12 @@ const side1 = Number(prompt("Please enter first side length"));
 const side2 = Number(prompt("Please enter second side length"));
 const hypotenous = Number(prompt("Please enter hypotenous length"));
 
+const realhyptenous = Number(Math.sqrt(side1 ** 2 + side2 ** 2));
+const isvalidrightangle = realhyptenous === hypotenous;
 
+if (isvalidrightangle){
+    alert("this is a right angel triangel")
+}
+else{
+    alert("this isn't a right angel triangel")
+}
--- module-ii/02-05012025-conditions/dist/app.js
+++ module-ii/02-05012025-conditions/dist/app.js
@@ -2,3 +2,11 @@ alert("let's check if your triangle is a valid right triangle");
 var side1 = Number(prompt("Please enter first side length"));
 var side2 = Number(prompt("Please enter second side length"));
 var hypotenous = Number(prompt("Please enter hypotenous length"));
+var realhyptenous = Number(Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2)));
+var isvalidrightangle = realhyptenous === hypotenous;
+if (isvalidrightangle) {
+    alert("this is a right angel triangel");
+}
+else {
+    alert("this isn't a right angel triangel");
+}
