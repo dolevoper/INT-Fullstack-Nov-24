563a3b08684d1da5d8ddea1f6a4e46ef073fc153
gregoryb79 <gregoryb79@gmail.com>
2025-01-05 19:45:37 +0200

    TallEnoughToRide - soution

--- module-ii/02-05012025-conditions/dist/tallEnoughToRide.js
+++ module-ii/02-05012025-conditions/dist/tallEnoughToRide.js
@@ -5,3 +5,14 @@ var userHeightInCm = Number(prompt("What is your height? (cm)"));
 // At least 140cm tall
 // Or above 18 y/o
 // Output: "You can board the ride!" or "Go home"
+if (userAge >= 18) {
+    alert("You can board the ride!");
+}
+else {
+    if ((userAge >= 8) && (userHeightInCm >= 140)) {
+        alert("You can board the ride!");
+    }
+    else {
+        alert("Go home");
+    }
+}
--- module-ii/02-05012025-conditions/tallEnoughToRide.ts
+++ module-ii/02-05012025-conditions/tallEnoughToRide.ts
@@ -7,3 +7,13 @@ const userHeightInCm = Number(prompt("What is your height? (cm)"));
 // Or above 18 y/o
 
 // Output: "You can board the ride!" or "Go home"
+
+if (userAge >= 18){
+    alert("You can board the ride!");
+} else{
+    if ((userAge >= 8) && (userHeightInCm >= 140)){
+        alert("You can board the ride!");
+    } else{
+        alert("Go home");
+    }
+}
