d2c0f4064e6c653051a399a55f7a311af542868a
gregoryb79 <gregoryb79@gmail.com>
2025-01-06 10:09:05 +0200

    cleanup

--- student-projects/gregory/Lecture 12/dist/apps.js
+++
@@ -1,5 +0,0 @@
-var favFood = prompt("What is your favorite food?");
-alert("Wow " + favFood + " is my favorite food too.");
-var num1 = Number(prompt("enter number"));
-var num2 = Number(prompt("enter another number"));
-alert(num1 + num2);
