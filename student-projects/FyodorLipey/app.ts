3ed500f89b07468fd71e2468ea58dcf8482238ff
FyodorLipey <fyodorikon@gmail.com>
2025-01-07 21:32:02 +0200

    guess game

--- student-projects/FyodorLipey/app.ts
+++ student-projects/FyodorLipey/app.ts
@@ -1,42 +1,60 @@
 
-function calc (number, operation, secNumber){
-    if (operation === "+"){
-       alert(Number(number + secNumber));
-       location.reload();
-    } else if (operation === "-") {
-        alert(Number(number - secNumber));
-        location.reload();
-    } else if (operation === "*"){
-        alert(Number(number * secNumber));
-        location.reload();
-    } else if (operation === "/"){
-        alert(Number(number / secNumber));
-        location.reload();
-    } else {
-        alert("Please choose: + - * / ")
-        location.reload();
-    }
-    }
-    
-const num1 =  Number(prompt("enter first number"));
-if (isNaN(num1)){
-alert("Please enter a number! Refreshing page")
+// function calc (number, operation, secNumber){
+//     if (operation === "+"){
+//        alert(Number(number + secNumber));
+//        location.reload();
+//     } else if (operation === "-") {
+//         alert(Number(number - secNumber));
+//         location.reload();
+//     } else if (operation === "*"){
+//         alert(Number(number * secNumber));
+//         location.reload();
+//     } else if (operation === "/"){
+//         alert(Number(number / secNumber));
+//         location.reload();
+//     } else {
+//         alert("Please choose: + - * / ")
+//         location.reload();
+//     }
+//     }
+  
+// const num1 =  Number(prompt("enter first number"));
+// if (isNaN(num1)){
+// alert("Please enter a number! Refreshing page")
+// location.reload();
+// } else if (!isNaN(num1)) {
+// const operation = prompt("choose operation");
+// if(operation !== "+" && operation !== "-" && operation !== "/" && operation !== "*" ){
+// alert("Please choose: + - * / ! Refreshing page");
+// location.reload();
+// }  else {
+// const num2 =  Number(prompt("enter second number"));
+// if (isNaN(num2)){
+//     alert("Please enter a number! Refreshing page");
+//      location.reload();
+// } else if(operation === "/" && num2 === 0) {
+//     alert("You cannot divide 0, refreshing");
+//     location.reload();
+// } else {
+// calc(num1, operation, num2);
+// }
+// }
+// }
+
+
+// Guess game 
+const randomNum = Math.floor(Math.random() * 100);
+const userNum = Number(prompt("Choose a number between 1 - 100"));
+
+function GuessGame (num1, num2){
+if (num1 === num2){
+alert("you won!");
 location.reload();
-} else if (!isNaN(num1)) {
-const operation = prompt("choose operation");
-if(operation !== "+" && operation !== "-" && operation !== "/" && operation !== "*" ){
-alert("Please choose: + - * / ! Refreshing page");
-location.reload();
-}  else {
-const num2 =  Number(prompt("enter second number"));
-if (isNaN(num2)){
-    alert("Please enter a number! Refreshing page");
-     location.reload();
-} else if(operation === "/" && num2 === 0) {
-    alert("You cannot divide 0, refreshing");
-    location.reload();
 } else {
-calc(num1, operation, num2);
+alert("try again, the number was " + num1);
+
+location.reload();   
 }
+
 }
-}
+GuessGame(randomNum, userNum); 
--- student-projects/FyodorLipey/dist/app.js
+++ student-projects/FyodorLipey/dist/app.js
@@ -1,48 +1,54 @@
-function calc(number, operation, secNumber) {
-    if (operation === "+") {
-        alert(Number(number + secNumber));
-        location.reload();
-    }
-    else if (operation === "-") {
-        alert(Number(number - secNumber));
-        location.reload();
-    }
-    else if (operation === "*") {
-        alert(Number(number * secNumber));
-        location.reload();
-    }
-    else if (operation === "/") {
-        alert(Number(number / secNumber));
+// function calc (number, operation, secNumber){
+//     if (operation === "+"){
+//        alert(Number(number + secNumber));
+//        location.reload();
+//     } else if (operation === "-") {
+//         alert(Number(number - secNumber));
+//         location.reload();
+//     } else if (operation === "*"){
+//         alert(Number(number * secNumber));
+//         location.reload();
+//     } else if (operation === "/"){
+//         alert(Number(number / secNumber));
+//         location.reload();
+//     } else {
+//         alert("Please choose: + - * / ")
+//         location.reload();
+//     }
+//     }
+// const num1 =  Number(prompt("enter first number"));
+// if (isNaN(num1)){
+// alert("Please enter a number! Refreshing page")
+// location.reload();
+// } else if (!isNaN(num1)) {
+// const operation = prompt("choose operation");
+// if(operation !== "+" && operation !== "-" && operation !== "/" && operation !== "*" ){
+// alert("Please choose: + - * / ! Refreshing page");
+// location.reload();
+// }  else {
+// const num2 =  Number(prompt("enter second number"));
+// if (isNaN(num2)){
+//     alert("Please enter a number! Refreshing page");
+//      location.reload();
+// } else if(operation === "/" && num2 === 0) {
+//     alert("You cannot divide 0, refreshing");
+//     location.reload();
+// } else {
+// calc(num1, operation, num2);
+// }
+// }
+// }
+// Guess game 
+var randomNum = Math.floor(Math.random() * 100);
+var userNum = Number(prompt("Choose a number between 1 - 100"));
+function GuessGame(num1, num2) {
+    if (num1 === num2) {
+        alert("you won!");
         location.reload();
     }
     else {
-        alert("Please choose: + - * / ");
+        alert("try again, the number was " + num1);
         location.reload();
     }
 }
-var num1 = Number(prompt("enter first number"));
-if (isNaN(num1)) {
-    alert("Please enter a number! Refreshing page");
-    location.reload();
-}
-else if (!isNaN(num1)) {
-    var operation = prompt("choose operation");
-    if (operation !== "+" && operation !== "-" && operation !== "/" && operation !== "*") {
-        alert("Please choose: + - * / ! Refreshing page");
-        location.reload();
-    }
-    else {
-        var num2 = Number(prompt("enter second number"));
-        if (isNaN(num2)) {
-            alert("Please enter a number! Refreshing page");
-            location.reload();
-        }
-        else if (operation === "/" && num2 === 0) {
-            alert("You cannot divide 0, refreshing");
-            location.reload();
-        }
-        else {
-            calc(num1, operation, num2);
-        }
-    }
-}
+GuessGame(randomNum, userNum);
