49f42a02fb50a3d2076ec6405dc5e85cec9db991
Kirill <makeshiftpost@gmail.com>
2025-01-08 16:22:33 +0200

    guessing game success !

--- student-projects/kirill/05012025-calculator/app.ts
+++ student-projects/kirill/05012025-calculator/app.ts
@@ -1,23 +1,44 @@
-alert("This is a calculator");
-const firstNumber = Number (prompt("Pick your first number"));
-const op = prompt("choose operator");
-   const addIt = Number("+");
-   const subtractIt = Number ("-");
-   const divideIt = Number ("/");
-   const multiplyIt = Number("*");
+// Calculator
+// alert("This is a calculator");
+// const firstNumber = Number (prompt("Pick your first number"));
+// const op = prompt("choose operator");
+//    const addIt = Number("+");
+//    const subtractIt = Number ("-");
+//    const divideIt = Number ("/");
+//    const multiplyIt = Number("*");
 
-const secondNumber = Number(prompt("Pick your second number"));
+// const secondNumber = Number(prompt("Pick your second number"));
 
-if (addIt){
-    alert(FirstNumber+secondNumber);
-} if (subtractIt) {
-    alert(firstNumber-secondNumber);
-    if(divideIt){
-        alert(firstNumber/secondNumber);
-        if(multiplyIt){
-            alert(firstNumber*secondNumber);
-        }
-    }
+// if (addIt){
+//     alert(FirstNumber+secondNumber);
+// }else if (subtractIt) {
+//     alert(firstNumber-secondNumber);
+//    else if(divideIt){
+//         alert(firstNumber/secondNumber);
+//        else if(multiplyIt){
+//             alert(firstNumber*secondNumber);
+//             else (isNaN(firstNumber, op, secondNumber)){
+//                 alert("This is not a number!");
+//             }
+//         }
+//     }
+// }
+
+//Guess a Number
+alert("try to guess my number (1-10)");
+const minNum = 1;
+const maxNum = 10;
+const answer = Math.floor(Math.random() * (maxNum - minNum +2)) + minNum;
+console.log(answer);
+const guess = Number(prompt("what is your number?"));
+if (guess===answer){
+    alert("Good guess");
+}
+if (isNaN(guess)){
+    alert("This is not a number!");
+}
+else if (guess !== answer) {
+    alert("wrong guess");
 }
 
 
--- student-projects/kirill/05012025-calculator/dist/app.js
+++ student-projects/kirill/05012025-calculator/dist/app.js
@@ -1,20 +1,39 @@
-alert("This is a calculator");
-var firstNumber = Number(prompt("Pick your first number"));
-var op = prompt("choose operator");
-var addIt = Number("+");
-var subtractIt = Number("-");
-var divideIt = Number("/");
-var multiplyIt = Number("*");
-var secondNumber = Number(prompt("Pick your second number"));
-if (addIt) {
-    alert(FirstNumber + secondNumber);
+// Calculator
+// alert("This is a calculator");
+// const firstNumber = Number (prompt("Pick your first number"));
+// const op = prompt("choose operator");
+//    const addIt = Number("+");
+//    const subtractIt = Number ("-");
+//    const divideIt = Number ("/");
+//    const multiplyIt = Number("*");
+// const secondNumber = Number(prompt("Pick your second number"));
+// if (addIt){
+//     alert(FirstNumber+secondNumber);
+// }else if (subtractIt) {
+//     alert(firstNumber-secondNumber);
+//    else if(divideIt){
+//         alert(firstNumber/secondNumber);
+//        else if(multiplyIt){
+//             alert(firstNumber*secondNumber);
+//             else (isNaN(firstNumber, op, secondNumber)){
+//                 alert("This is not a number!");
+//             }
+//         }
+//     }
+// }
+//Guess a Number
+alert("try to guess my number (1-10)");
+var minNum = 1;
+var maxNum = 10;
+var answer = Math.floor(Math.random() * (maxNum - minNum + 2)) + minNum;
+console.log(answer);
+var guess = Number(prompt("what is your number?"));
+if (guess === answer) {
+    alert("Good guess");
 }
-if (subtractIt) {
-    alert(firstNumber - secondNumber);
-    if (divideIt) {
-        alert(firstNumber / secondNumber);
-        if (multiplyIt) {
-            alert(firstNumber * secondNumber);
-        }
-    }
+if (isNaN(guess)) {
+    alert("This is not a number!");
+}
+else if (guess !== answer) {
+    alert("wrong guess");
 }
