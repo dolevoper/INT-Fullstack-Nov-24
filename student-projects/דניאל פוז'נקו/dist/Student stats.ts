da598eb648996f56c77fba5f5e1e2d9bf7eb3f71
Daniel Puzhenko <danpuz35@gmail.com>
2025-01-11 13:29:54 +0200

    feat: add Student stats HTML and TypeScript files, implement basic operation prompts, and remove old guessing game functionality

---
+++ student-projects/דניאל פוז'נקו/dist/Student stats.ts
@@ -0,0 +1,15 @@
+let userInputName = 0;
+let userInputGrade = 0;
+const operation = prompt('Enter operation: min , max');
+
+switch (operation) {
+    case 'min':
+     
+        break;
+    case 'max':
+  
+        break;
+    default:
+        console.log('Invalid operation');
+        break;
+}
---
+++ student-projects/דניאל פוז'נקו/dist/dist/Student stats.js
@@ -0,0 +1,12 @@
+var userInputName = 0;
+var userInputGrade = 0;
+var operation = prompt('Enter operation: min , max');
+switch (operation) {
+    case 'min':
+        break;
+    case 'max':
+        break;
+    default:
+        console.log('Invalid operation');
+        break;
+}
