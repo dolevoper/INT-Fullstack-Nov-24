0b6eb0fff387448e2d429239316dcf9bf366527d
Kirill <makeshiftpost@gmail.com>
2025-01-11 22:34:15 +0200

    failed second assignment

---
+++ student-projects/kirill-08012025-loops/app.ts
@@ -0,0 +1,31 @@
+// let sumNumbers = 0;
+
+// const numbersToRead = Number(prompt("how many numbers do you want to input?"));
+//  if (numbersToRead === null){
+//     alert("reset");
+//  }
+// for (let i = 0; i < numbersToRead; i++) {
+//     const numberToAdd = Number(prompt("please enter a number"));
+
+//     // sumNumbers = sumNumbers + numberToAdd
+//     sumNumbers += numberToAdd;
+
+// }
+
+
+// const average = sumNumbers / numbersToRead;
+
+// alert(average);
+
+//Student Grade
+const max = 100;
+const min = 0;
+
+let userName = prompt("Enter your name");
+let minMax = prompt("Which operation would you like to perform?");
+
+
+
+
+
+
---
+++ student-projects/kirill-08012025-loops/dist/app.js
@@ -0,0 +1,17 @@
+// let sumNumbers = 0;
+// const numbersToRead = Number(prompt("how many numbers do you want to input?"));
+//  if (numbersToRead === null){
+//     alert("reset");
+//  }
+// for (let i = 0; i < numbersToRead; i++) {
+//     const numberToAdd = Number(prompt("please enter a number"));
+//     // sumNumbers = sumNumbers + numberToAdd
+//     sumNumbers += numberToAdd;
+// }
+// const average = sumNumbers / numbersToRead;
+// alert(average);
+//Student Grade
+var max = 100;
+var min = 0;
+var userName = prompt("Enter your name");
+var minMax = prompt("Which operation would you like to perform?");
