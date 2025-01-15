f2986613a0fb174697a02fc093ba06b27c40ea85
YaelMeydan <yael.meydan@gmail.com>
2025-01-15 20:05:41 +0200

    15-01 game entry

--- student-projects/YaelMeydan/15-01.ts
+++ student-projects/YaelMeydan/15-01.ts
@@ -1,3 +1,3 @@
 const UserNickname = prompt("Hello, welcome to the ___ game.\n Please enter your nickname:");
-const Game
+const GameEnrtyScreen = prompt("Hi," + UserNickname + "This game is____.\n Press 'ok' to start")
 
--- student-projects/YaelMeydan/dist/15-01.js
+++ student-projects/YaelMeydan/dist/15-01.js
@@ -1,2 +1,2 @@
 var UserNickname = prompt("Hello, welcome to the ___ game.\n Please enter your nickname:");
-var Game;
+var GameEnrtyScreen = prompt("Hi," + UserNickname + "This game is____.\n Press 'ok' to start");
