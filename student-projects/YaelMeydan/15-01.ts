206c5b679bd3b0085fd0efa83b774079846e57e1
YaelMeydan <yael.meydan@gmail.com>
2025-01-15 21:21:03 +0200

    15-01 gameentermode

--- student-projects/YaelMeydan/15-01.ts
+++ student-projects/YaelMeydan/15-01.ts
@@ -1,3 +1,8 @@
-const UserNickname = prompt("Hello, welcome to the ___ game.\n Please enter your nickname:");
-const GameEnrtyScreen = prompt("Hi," + UserNickname + "This game is____.\n Press 'ok' to start")
+const UserNickname = prompt("Hello, welcome to the ___ game.\nPlease enter your nickname:");
+confirm("Hi,  " + UserNickname + " This game is____.\nPress 'ok' to start or 'cancel' at any time to quit the game.");
+playgame()
+function playgame(){
+    prompt("You are now in the ___.\n Please type which ___ you would like to begin with:\n 1 / 2 / 3?");
+
+}
 
--- student-projects/YaelMeydan/dist/15-01.js
+++ student-projects/YaelMeydan/dist/15-01.js
@@ -1,2 +1,6 @@
-var UserNickname = prompt("Hello, welcome to the ___ game.\n Please enter your nickname:");
-var GameEnrtyScreen = prompt("Hi," + UserNickname + "This game is____.\n Press 'ok' to start");
+var UserNickname = prompt("Hello, welcome to the ___ game.\nPlease enter your nickname:");
+confirm("Hi,  " + UserNickname + " This game is____.\nPress 'ok' to start or 'cancel' at any time to quit the game.");
+playgame();
+function playgame() {
+    prompt("You are now in the ___.\n Please type which ___ you would like to begin with:\n 1 / 2 / 3?");
+}
