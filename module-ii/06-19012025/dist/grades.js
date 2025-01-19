a1c884bca245af6fd9fe21f9f773a52142497aac
Guy <Guy9502@Gmail.com>
2025-01-19 20:22:48 +0200

    test

--- module-ii/06-19012025/dist/grades.js
+++ module-ii/06-19012025/dist/grades.js
@@ -1,7 +1,23 @@
-var numbers = [];
-for (var i = 0; i < 10; i++) {
-    numbers[i] = Number(prompt("Please enter a number"));
+// const numbers: number[] = [];
+// for (let i = 0; i < 10; i++) {
+//     numbers[i] = Number(prompt("Please enter a number"));
+// }
+// for (let i = 0; i < 10; i++) {
+//     alert(numbers[i]);
+// }
+function getNumbers() {
+    var toContinue = true;
+    var number2 = [];
+    var index = 0;
+    while (toContinue) {
+        number2[index] = Number(prompt("Please enter a number"));
+        index++;
+        toContinue = confirm("Do you want to continue?");
+    }
+    //print the numbers
+    alert(number2.join(", "));
+    // for (let i = 0; i < number2.length; i++) {
+    //     alert(number2[i]);
+    // }
 }
-for (var i = 0; i < 10; i++) {
-    alert(numbers[i]);
-}
+getNumbers();
--- module-ii/06-19012025/grades.ts
+++ module-ii/06-19012025/grades.ts
@@ -1,9 +1,27 @@
-const numbers: number[] = [];
+// const numbers: number[] = [];
 
-for (let i = 0; i < 10; i++) {
-    numbers[i] = Number(prompt("Please enter a number"));
-}
+// for (let i = 0; i < 10; i++) {
+//     numbers[i] = Number(prompt("Please enter a number"));
+// }
 
-for (let i = 0; i < 10; i++) {
-    alert(numbers[i]);
+// for (let i = 0; i < 10; i++) {
+//     alert(numbers[i]);
+// }
+
+function getNumbers(){
+    let toContinue = true;
+    const number2: number[] = [];
+    let index = 0;
+    while (toContinue) {
+        number2[index] = Number(prompt("Please enter a number"));
+        index++;
+        toContinue = confirm("Do you want to continue?");
+    }
+
+    //print the numbers
+    alert(number2.join(", "));
+    // for (let i = 0; i < number2.length; i++) {
+    //     alert(number2[i]);
+    // }
 }
+getNumbers();
