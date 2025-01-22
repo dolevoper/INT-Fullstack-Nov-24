b28b689e45a41dda9b00a8fe81e9b2aef8bdba99
amit reuveni <amitr123r@gmail.com>
2025-01-22 17:33:16 +0200

    amit reuveni

---
+++ student-projects/amit_reuveni_Task03/readNumbers.practice.ts
@@ -0,0 +1,71 @@
+// 2. Read 10 numbers. Alert all the numbers in ascending order (one alert or multiple) (don't use array methods)
+
+
+const num1 = Number(prompt("Please enter a number"));
+const num2 = Number(prompt("Please enter a number"));
+const num3 = Number(prompt("Please enter a number"));
+const num4 = Number(prompt("Please enter a number"));
+const num5 = Number(prompt("Please enter a number"));
+const num6 = Number(prompt("Please enter a number"));
+const num7 = Number(prompt("Please enter a number"));
+const num8 = Number(prompt("Please enter a number"));
+const num9 = Number(prompt("Please enter a number"));
+const num10 = Number(prompt("Please enter a number"));
+
+let renum = 0;
+
+if (num1 > num2) { renum = num1; num1 = num2; num2 = renum; }
+if (num1 > num3) { renum = num1; num1 = num3; num3 = renum; }
+if (num1 > num4) { renum = num1; num1 = num4; num4 = renum; }
+if (num1 > num5) { renum = num1; num1 = num5; num5 = renum; }
+if (num1 > num6) { renum = num1; num1 = num6; num6 = renum; }
+if (num1 > num7) { renum = num1; num1 = num7; num7 = renum; }
+if (num1 > num8) { renum = num1; num1 = num8; num8 = renum; }
+if (num1 > num9) { renum = num1; num1 = num9; num9 = renum; }
+if (num1 > num10) { renum = num1; num1 = num10; num10 = renum; }
+
+if (num2 > num3) { renum = num2; num2 = num3; num3 = renum; }
+if (num2 > num4) { renum = num2; num2 = num4; num4 = renum; }
+if (num2 > num5) { renum = num2; num2 = num5; num5 = renum; }
+if (num2 > num6) { renum = num2; num2 = num6; num6 = renum; }
+if (num2 > num7) { renum = num2; num2 = num7; num7 = renum; }
+if (num2 > num8) { renum = num2; num2 = num8; num8 = renum; }
+if (num2 > num9) { renum = num2; num2 = num9; num9 = renum; }
+if (num2 > num10) { renum = num2; num2 = num10; num10 = renum; }
+
+if (num3 > num4) { renum = num3; num3 = num4; num4 = renum; }
+if (num3 > num5) { renum = num3; num3 = num5; num5 = renum; }
+if (num3 > num6) { renum = num3; num3 = num6; num6 = renum; }
+if (num3 > num7) { renum = num3; num3 = num7; num7 = renum; }
+if (num3 > num8) { renum = num3; num3 = num8; num8 = renum; }
+if (num3 > num9) { renum = num3; num3 = num9; num9 = renum; }
+if (num3 > num10) { renum = num3; num3 = num10; num10 = renum; }
+
+if (num4 > num5) { renum = num4; num4 = num5; num5 = renum; }
+if (num4 > num6) { renum = num4; num4 = num6; num6 = renum; }
+if (num4 > num7) { renum = num4; num4 = num7; num7 = renum; }
+if (num4 > num8) { renum = num4; num4 = num8; num8 = renum; }
+if (num4 > num9) { renum = num4; num4 = num9; num9 = renum; }
+if (num4 > num10) { renum = num4; num4 = num10; num10 = renum; }
+
+if (num5 > num6) { renum = num5; num5 = num6; num6 = renum; }
+if (num5 > num7) { renum = num5; num5 = num7; num7 = renum; }
+if (num5 > num8) { renum = num5; num5 = num8; num8 = renum; }
+if (num5 > num9) { renum = num5; num5 = num9; num9 = renum; }
+if (num5 > num10) { renum = num5; num5 = num10; num10 = renum; }
+
+if (num6 > num7) { renum = num6; num6 = num7; num7 = renum; }
+if (num6 > num8) { renum = num6; num6 = num8; num8 = renum; }
+if (num6 > num9) { renum = num6; num6 = num9; num9 = renum; }
+if (num6 > num10) { renum = num6; num6 = num10; num10 = renum; }
+
+if (num7 > num8) { renum = num7; num7 = num8; num8 = renum; }
+if (num7 > num9) { renum = num7; num7 = num9; num9 = renum; }
+if (num7 > num10) { renum = num7; num7 = num10; num10 = renum; }
+
+if (num8 > num9) { renum = num8; num8 = num9; num9 = renum; }
+if (num8 > num10) { renum = num8; num8 = num10; num10 = renum; }
+
+if (num9 > num10) { renum = num9; num9 = num10; num10 = renum; }
+
+alert(`The numbers in ascending order are: ${num1}, ${num2}, ${num3}, ${num4}, ${num5}, ${num6}, ${num7}, ${num8}, ${num9}, ${num10}`);
