57b862b0192c666010926c71b2ed91ec285d513c
FyodorLipey <fyodorikon@gmail.com>
2025-01-28 23:08:44 +0200

    fix with sale/rent and trying .filter method

--- student-projects/fyodor-lipey/CAR COMPANY/cars.ts
+++ student-projects/fyodor-lipey/CAR COMPANY/cars.ts
@@ -4,39 +4,37 @@ type car = {
   year: number;
   kilometerage: number;
   hand: number;
+  purpose: string | undefined;
 };
 
-const carForSale: car[] = [];
-const carForRent: car[] = [];
 const allCars: car[] = [];
 menu();
 
 function menu() {
-let currentState: Function | undefined = CarCompany;
+  let currentState: Function | undefined = CarCompany;
 
- while (currentState){
+  while (currentState) {
     currentState = currentState();
- }
+  }
 
-alert("Exiting current program and reloading");
-location.reload();
-
+  alert("Exiting current program and reloading");
+  location.reload();
 }
 function errorHandling() {
-    const exitAnswer = prompt("are you sure you want to quit? YES/NO");
-    switch (exitAnswer?.trim().toLowerCase()) {
-      case "yes":
-      case "y":
-        return;
-      case "no":
-      case "n":
-          return CarCompany();
-      default:
-        alert("Please choose YES or NO");
-        return errorHandling();
-    }
+  const exitAnswer = prompt("are you sure you want to quit? YES/NO");
+  switch (exitAnswer?.trim().toLowerCase()) {
+    case "yes":
+    case "y":
+      return;
+    case "no":
+    case "n":
+      return carCompany();
+    default:
+      alert("Please choose YES or NO");
+      return errorHandling();
   }
-function CarCompany() {
+}
+function carCompany() {
   const userInput = prompt(
     "Welcome! Please choose what you want to do: \n" +
       "Register a new car - enter REGISTER or 1 \n" +
@@ -49,29 +47,28 @@ function CarCompany() {
     case "register":
     case "1":
     case "new":
-      return registerNewCar(carForSale, carForRent, allCars);
-    case "find":
-    case "2":
-      return findCar(allCars);
-    case "status":
-    case "3":
-      return carStatus(carForSale, carForRent, allCars);
-    case "exit":  
+      return registerNewCar;
+    case "exit":
     case undefined:
-         errorHandling;
+      errorHandling;
     default:
-    alert("unknow action"); 
-    return CarCompany;
+      alert("unknow action");
+      return carCompany;
   }
 }
 
-function registerNewCar(arraySale: car[],arrayRent: car[],arrayAll: car[]){
+function stringCar(array: car[]) {
+  return array.toString();
+}
+
+function registerNewCar() {
   alert("Register a new car");
   const manufacturer = String(prompt("Enter manufacturer:"));
   const model = Number(prompt("Enter the model"));
   const year = Number(prompt("Enter year"));
   const kilometerage = Number(prompt("Enter the kilometerage"));
   const hand = Number(prompt("Enter number of previous owners (in numbers)"));
+  const purpose = String(prompt("Sale/Rent"));
 
   const newCar: car = {
     manufacturer: manufacturer,
@@ -79,64 +76,58 @@ function registerNewCar(arraySale: car[],arrayRent: car[],arrayAll: car[]){
     year: year,
     kilometerage: kilometerage,
     hand: hand,
+    purpose: purpose,
   };
-  const saleOrRent = prompt("Choose if a car for SALE/RENT");
-  switch (saleOrRent?.trim().toLowerCase()) {
-    case "sale":
-      arraySale.push(newCar);
-      arrayAll.push(newCar);
-    case "rent":
-      arrayRent.push(newCar);
-      arrayAll.push(newCar);
+  allCars.push(newCar);
+  const userInput = prompt(
+    "return to menu or register another car?\n" +
+      'enter "New" for new registration or "Return" in order to return to main menu'
+  );
+  switch (userInput?.trim().toLowerCase()) {
+    case "return":
+    case "new":
+      return registerNewCar;
+    case "return":
+    case "back":
+      return menu;
     case undefined:
+      errorHandling;
     default:
-      arrayAll.push(newCar);
+      alert("unknow action");
+      return menu;
   }
-  alert(JSON.stringify(newCar))
-  return menu;
 }
 
-function findCar(arrayAll: car[]) {
-  const foundCars: car [] = []; 
+function findCar() {
+  const foundCars: car[] = [];
   alert("Find a car - enter manufacturer and model");
   const manufacturer = String(prompt("Enter manufacturer:"));
   const model = Number(prompt("Enter the model"));
-  for (let i = 0; i < arrayAll.length; i++){
-  if (arrayAll[i].manufacturer === manufacturer && arrayAll[i].model === model){
-  foundCars.push(arrayAll[i]);
-  } 
-}
-  if (foundCars[0] === null) {
+  for (let i = 0; i < allCars.length; i++) {
+    if (
+      allCars[i].manufacturer === manufacturer &&
+      allCars[i].model === model
+    ) {
+      foundCars.push(allCars[i]);
+    }
+  }
+  if (!foundCars.length) {
     alert("No match");
     return;
   } else {
-    alert(JSON.stringify(foundCars));
+    alert(foundCars.toString());
     return foundCars;
   }
 }
 
-function carStatus(arraySale: car[],arrayRent: car[],arrayAll: car[]){
-const searchOption = prompt("choose which list you need: sale/rent/all")
-switch (searchOption?.trim().toLowerCase()) {
-    case "sale":
-      alert(JSON.stringify(arraySale));
-      return arraySale;
-    case "rent":
-        alert(JSON.stringify(arrayRent));
-        return arrayRent;
-    case "all":
-    default:
-        alert(JSON.stringify(arrayAll));
-        return arrayAll;
-    case undefined:
-        errorHandling; 
+function carStatus() {
+  const searchOption = prompt("choose which list you need: sale/rent/all");
+  let purposeCars: car[] = [];
+  if (searchOption?.trim().toLowerCase() === "sale") {
+    purposeCars = allCars.filter((car) => car.purpose === "sale");
+  } else if (searchOption?.trim().toLowerCase() === "rent") {
+    purposeCars = allCars.filter((car) => car.purpose === "rent");
+  } else {
+  return alert(allCars.toString());
   }
-  
-}
-
-function oldestCar(arrayAll: car []){
-for (const arrayAll of arrayAll){
-
-}
-
 }
--- student-projects/fyodor-lipey/CAR COMPANY/dist/cars.js
+++ student-projects/fyodor-lipey/CAR COMPANY/dist/cars.js
@@ -1,5 +1,3 @@
-var carForSale = [];
-var carForRent = [];
 var allCars = [];
 menu();
 function menu() {
@@ -18,13 +16,13 @@ function errorHandling() {
             return;
         case "no":
         case "n":
-            return CarCompany();
+            return carCompany();
         default:
             alert("Please choose YES or NO");
             return errorHandling();
     }
 }
-function CarCompany() {
+function carCompany() {
     var userInput = prompt("Welcome! Please choose what you want to do: \n" +
         "Register a new car - enter REGISTER or 1 \n" +
         "To find an existing car - enter FIND or 2 \n" +
@@ -34,88 +32,81 @@ function CarCompany() {
         case "register":
         case "1":
         case "new":
-            return registerNewCar(carForSale, carForRent, allCars);
-        case "find":
-        case "2":
-            return findCar(allCars);
-        case "status":
-        case "3":
-            return carStatus(carForSale, carForRent, allCars);
+            return registerNewCar;
         case "exit":
         case undefined:
             errorHandling;
         default:
             alert("unknow action");
-            return CarCompany;
+            return carCompany;
     }
 }
-function registerNewCar(arraySale, arrayRent, arrayAll) {
+function stringCar(array) {
+    return array.toString();
+}
+function registerNewCar() {
     alert("Register a new car");
     var manufacturer = String(prompt("Enter manufacturer:"));
     var model = Number(prompt("Enter the model"));
     var year = Number(prompt("Enter year"));
     var kilometerage = Number(prompt("Enter the kilometerage"));
     var hand = Number(prompt("Enter number of previous owners (in numbers)"));
+    var purpose = String(prompt("Sale/Rent"));
     var newCar = {
         manufacturer: manufacturer,
         model: model,
         year: year,
         kilometerage: kilometerage,
-        hand: hand
+        hand: hand,
+        purpose: purpose
     };
-    var saleOrRent = prompt("Choose if a car for SALE/RENT");
-    switch (saleOrRent === null || saleOrRent === void 0 ? void 0 : saleOrRent.trim().toLowerCase()) {
-        case "sale":
-            arraySale.push(newCar);
-            arrayAll.push(newCar);
-        case "rent":
-            arrayRent.push(newCar);
-            arrayAll.push(newCar);
+    allCars.push(newCar);
+    var userInput = prompt("return to menu or register another car?\n" +
+        'enter "New" for new registration or "Return" in order to return to main menu');
+    switch (userInput === null || userInput === void 0 ? void 0 : userInput.trim().toLowerCase()) {
+        case "return":
+        case "new":
+            return registerNewCar;
+        case "return":
+        case "back":
+            return menu;
         case undefined:
+            errorHandling;
         default:
-            arrayAll.push(newCar);
+            alert("unknow action");
+            return menu;
     }
-    alert(JSON.stringify(newCar));
-    return menu;
 }
-function findCar(arrayAll) {
+function findCar() {
     var foundCars = [];
     alert("Find a car - enter manufacturer and model");
     var manufacturer = String(prompt("Enter manufacturer:"));
     var model = Number(prompt("Enter the model"));
-    for (var i = 0; i < arrayAll.length; i++) {
-        if (arrayAll[i].manufacturer === manufacturer && arrayAll[i].model === model) {
-            foundCars.push(arrayAll[i]);
+    for (var i = 0; i < allCars.length; i++) {
+        if (allCars[i].manufacturer === manufacturer &&
+            allCars[i].model === model) {
+            foundCars.push(allCars[i]);
         }
     }
-    if (foundCars[0] === null) {
+    if (!foundCars.length) {
         alert("No match");
         return;
     }
     else {
-        alert(JSON.stringify(foundCars));
+        alert(foundCars.toString());
         return foundCars;
     }
 }
-function carStatus(arraySale, arrayRent, arrayAll) {
+function carStatus() {
     var searchOption = prompt("choose which list you need: sale/rent/all");
-    switch (searchOption === null || searchOption === void 0 ? void 0 : searchOption.trim().toLowerCase()) {
-        case "sale":
-            alert(JSON.stringify(arraySale));
-            return arraySale;
-        case "rent":
-            alert(JSON.stringify(arrayRent));
-            return arrayRent;
-        case "all":
-        default:
-            alert(JSON.stringify(arrayAll));
-            return arrayAll;
-        case undefined:
-            errorHandling;
+    var purposeCars = [];
+    if ((searchOption === null || searchOption === void 0 ? void 0 : searchOption.trim().toLowerCase()) === "sale") {
+        purposeCars = allCars.filter(function (car) { return car.purpose === "sale"; });
     }
-}
-function oldestCar(arrayAll) {
-    for (var _i = 0, arrayAll_1 = arrayAll_2; _i < arrayAll_1.length; _i++) {
-        var arrayAll_2 = arrayAll_1[_i];
+    else if ((searchOption === null || searchOption === void 0 ? void 0 : searchOption.trim().toLowerCase()) === "rent") {
+        purposeCars = allCars.filter(function (car) { return car.purpose === "rent"; });
+    }
+    else {
+        return alert(allCars.toString());
     }
 }
