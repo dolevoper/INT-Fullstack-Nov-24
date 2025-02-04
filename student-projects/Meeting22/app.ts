d560801e00eedb230f13ed04f0073e7123734ad2
Coral Tzubery <tzubery.coral@gmail.com>
2025-02-04 14:31:19 -0800

    meeting 22 - home project - statring the nim game

---
+++ student-projects/Meeting22/app.ts
@@ -0,0 +1,7 @@
+//create the game of Nim
+//two players
+//one player against the computer
+//the user choose how many matches to play with against the computer
+
+let heap = 10;
+let player: "player 1" | "player 2" = "player 1";
---
+++ student-projects/Meeting22/dist/app.js
@@ -0,0 +1,6 @@
+//create the game of Nim
+//two players
+//one player against the computer
+//the user choose how many matches to play with against the computer
+let heap = 10;
+let player = "player 1";
