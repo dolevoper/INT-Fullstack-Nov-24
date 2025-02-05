e1e5d2422d9211640865bad834343986c8f1971b
Coral Tzubery <tzubery.coral@gmail.com>
2025-02-04 17:06:37 -0800

    meeting 22 home project - adding styles and local storage of the wins of each player

--- student-projects/Meeting22/app.ts
+++ student-projects/Meeting22/app.ts
@@ -5,12 +5,27 @@ const confirmButton = document.createElement("button");
 confirmButton.textContent = "Remove matches";
 document.body.appendChild(confirmButton);
 
+const player1WinsDisplay = document.getElementById("player1-wins")
+const player2WinsDisplay = document.getElementById("player2-wins")
+
 let player: "player 1" | "player 2" = "player 1";
 let heapSize = 9;
 let selectedMatches: HTMLButtonElement[] = [];
 
+let player1Wins = parseInt(localStorage.getItem("player1Wins") || "0");
+let player2Wins = parseInt(localStorage.getItem("player2Wins") || "0");
+
 const gameOver = () => {
     alert(`${player} wins!`);
+    if (player === "player 1") {
+        player1Wins++;
+        localStorage.setItem("player1Wins", player1Wins.toString());
+        player1WinsDisplay.textContent = `Player 1: ${player1Wins} wins`;
+    } else {
+        player2Wins++;
+        localStorage.setItem("player2Wins", player2Wins.toString());
+        player2WinsDisplay.textContent = `Player 2: ${player1Wins} wins`;
+    }
     reset();
 };
 
@@ -22,8 +37,14 @@ const reset = () => {
     heapSize = 9;
     selectedMatches = [];
     player = "player 1";
+    playersDisplay();
 };
 
+const playersDisplay = () => {
+    const playerDisplay = document.getElementById("current-player");
+    playerDisplay.textContent = `It is ${player}'s turn`;
+
+};
 
 matches.forEach(match => {
     match.addEventListener("click", event => {
@@ -48,6 +69,7 @@ confirmButton.addEventListener("click", () => {
     }
 
     player = player == "player 1" ? "player 2" : "player 1";
+    playersDisplay();
 
     selectedMatches.forEach(button => {
         button.disabled = true;
--- student-projects/Meeting22/dist/app.js
+++ student-projects/Meeting22/dist/app.js
@@ -2,11 +2,25 @@ const matches = document.querySelectorAll(".match");
 const confirmButton = document.createElement("button");
 confirmButton.textContent = "Remove matches";
 document.body.appendChild(confirmButton);
+const player1WinsDisplay = document.getElementById("player1-wins");
+const player2WinsDisplay = document.getElementById("player2-wins");
 let player = "player 1";
 let heapSize = 9;
 let selectedMatches = [];
+let player1Wins = parseInt(localStorage.getItem("player1Wins") || "0");
+let player2Wins = parseInt(localStorage.getItem("player2Wins") || "0");
 const gameOver = () => {
     alert(`${player} wins!`);
+    if (player === "player 1") {
+        player1Wins++;
+        localStorage.setItem("player1Wins", player1Wins.toString());
+        player1WinsDisplay.textContent = `Player 1: ${player1Wins} wins`;
+    }
+    else {
+        player2Wins++;
+        localStorage.setItem("player2Wins", player2Wins.toString());
+        player2WinsDisplay.textContent = `Player 2: ${player1Wins} wins`;
+    }
     reset();
 };
 const reset = () => {
@@ -17,6 +31,11 @@ const reset = () => {
     heapSize = 9;
     selectedMatches = [];
     player = "player 1";
+    playersDisplay();
+};
+const playersDisplay = () => {
+    const playerDisplay = document.getElementById("current-player");
+    playerDisplay.textContent = `It is ${player}'s turn`;
 };
 matches.forEach(match => {
     match.addEventListener("click", event => {
@@ -37,6 +56,7 @@ confirmButton.addEventListener("click", () => {
         return;
     }
     player = player == "player 1" ? "player 2" : "player 1";
+    playersDisplay();
     selectedMatches.forEach(button => {
         button.disabled = true;
     });
