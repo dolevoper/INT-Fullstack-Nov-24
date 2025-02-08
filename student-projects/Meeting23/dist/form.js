4814e045617db6abafbdafa176612e36469f80a9
Coral Tzubery <tzubery.coral@gmail.com>
2025-02-08 12:50:41 -0800

    meeting23 - home project - adding the html code for the form

---
+++ student-projects/Meeting23/dist/form.js
@@ -0,0 +1,14 @@
+//checkbox code
+// const form = document.querySelector("#hobbiesForm") as HTMLFormElement;
+// const output = document.querySelector("#output") as HTMLElement;
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault(); // מונע רענון דף
+//   // שליפת כל ה-checkbox-ים המסומנים
+//   const selectedHobbies = Array.from(
+//     form.querySelectorAll("input[name='hobby']:checked")
+//   ).map(input => (input as HTMLInputElement).value);
+//   // הצגת הנתונים
+//   output.textContent = selectedHobbies.length
+//     ? `התחביבים שבחרת: ${selectedHobbies.join(", ")}`
+//     : "לא בחרת שום תחביב.";
+// });
