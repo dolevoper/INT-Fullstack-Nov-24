89a49cc9957256defded3c9a068b6f656a90dfef
YaelMeydan <yael.meydan@gmail.com>
2025-02-09 19:31:33 +0200

    inventory 9-2

---
+++ student-projects/YaelMeydan/dist/09-02.js
@@ -0,0 +1,9 @@
+export let items = [];
+export let stock = [];
+export let inventory = [];
+export function addItem(item) {
+}
+export function addToStock(itemId, amount) { }
+export function consume(itemId, quantity) { }
+export function acquire(itemId, serial) { }
+export function decomission(serial) { }
