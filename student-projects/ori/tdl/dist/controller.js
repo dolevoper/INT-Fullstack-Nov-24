08fbd090bcf21456f6e4bc0e7b41ec4d094eab12
Mental-potato <153oorrii153@gmail.com>
2025-02-26 15:50:52 +0200

    yes

--- student-projects/ori/tdl/dist/controller.js
+++ student-projects/ori/tdl/dist/controller.js
@@ -1,30 +1,21 @@
 import { addTodo, getTodos, initTodos, toggleTodo } from "./model.js";
-
 const todosStorageKey = "todos";
-
 export function init() {
     const todos = JSON.parse(localStorage.getItem(todosStorageKey)) ?? [];
-
     initTodos(todos);
 }
-
 export function save() {
     localStorage.setItem(todosStorageKey, JSON.stringify(getTodos()));
 }
-
-export function onAddTodoSubmit(formData: FormData) {
+export function onAddTodoSubmit(formData) {
     const rawContent = formData.get("content");
-        
     if (typeof rawContent !== "string") {
         throw new Error("Content must be a string");
     }
-
     const content = rawContent.trim();
-
     if (!content) {
         throw new Error("Content can't be empty");
     }
-
     addTodo({
         id: crypto.randomUUID().replaceAll("-", "").slice(-8),
         createdAt: new Date().valueOf(),
@@ -32,5 +23,4 @@ export function onAddTodoSubmit(formData: FormData) {
         content,
     });
 }
-
 export const onToggleTodo = toggleTodo;
