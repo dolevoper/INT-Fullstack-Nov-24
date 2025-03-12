768061d508fdb86e79be155371b62eb5d4d92a98
Omer Dolev <omerdolev90@gmail.com>
2025-03-12 21:18:50 +0200

    server stuff

--- module-iii/02-12032025-server-and-async/index.ts
+++ module-iii/02-12032025-server-and-async/index.ts
@@ -6,9 +6,41 @@ const server = createServer((req, res) => {
         res.end();
         return;
     }
-
-    console.log(req.method, req.url);
     
+    console.log(req.method, req.url);
+
+    if (req.url === "/image.png") {
+        readFile("./image.png", (err, content) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, "Oops, something went wrong...");
+                res.end();
+                return;
+            }
+    
+            res.write(content);
+            res.end();
+        });
+
+        return;
+    }
+
+    if (req.url === "/about.html") {
+        readFile("./about.html", (err, content) => {
+            if (err) {
+                console.error(err);
+                res.writeHead(500, "Oops, something went wrong...");
+                res.end();
+                return;
+            }
+    
+            res.write(content);
+            res.end();
+        });
+
+        return;
+    }
+
     readFile("./index.html", (err, content) => {
         if (err) {
             console.error(err);
