7f11356a39308dc344a82d1870449c585b259ed1
Omer Dolev <omerdolev90@gmail.com>
2025-03-12 20:51:51 +0200

    more server stuff

--- module-iii/02-12032025-server-and-async/index.ts
+++ module-iii/02-12032025-server-and-async/index.ts
@@ -1,12 +1,25 @@
+import { readFile } from "fs";
 import { createServer } from "http";
 
 const server = createServer((req, res) => {
-    if (req.url !== "/favicon.ico") {
-        console.log(req.method, req.url);
+    if (req.url === "/favicon.ico") {
+        res.end();
+        return;
     }
 
-    res.write("<h1>hello html</h1>");
-    res.end();
+    console.log(req.method, req.url);
+    
+    readFile("./index.html", (err, content) => {
+        if (err) {
+            console.error(err);
+            res.writeHead(500, "Oops, something went wrong...");
+            res.end();
+            return;
+        }
+
+        res.write(content);
+        res.end();
+    });
 });
 
 server.listen(8080, () => console.log("server is listening on port 8080"));
